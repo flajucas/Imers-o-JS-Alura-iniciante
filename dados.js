1776a6cb0dc9aa9573b636d0ecb41cc86f1cac1e
flajucas <116657737+flajucas@users.noreply.github.com>
2024-09-10 01:50:43 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,25 @@
+let dados = [
+    {
+        titulo: "Culinária Japonesa",
+        descricao: "A culinária japonesa, além de ser uma delícia para o paladar, é uma verdadeira expressão artística. Caracterizada por sua frescura, simplicidade e equilíbrio de sabores, a gastronomia japonesa valoriza ingredientes de alta qualidade e técnicas de preparo milenares. Desde o tradicional sushi, com suas infinitas variações, até os saborosos ramen e os delicados sashimis, a culinária japonesa oferece uma experiência gastronômica única, capaz de capaz de encantar os mais exigentes paladares. A apresentação dos pratos também é um ponto forte, com cada detalhe cuidadosamente pensado para criar uma experiência visual e sensorial completa.",
+        tags: "comida japão japonesa cozinha",
+    },
+    {
+        titulo: "Ingredientes para Sushi",
+        descricao: "Arroz para sushi, vinagre de arroz, açúcar, sal, nori (alga marinha), peixe cru (atum, salmão, etc.), frutos do mar (camarão, polvo, etc.), ovo, pepino, abacate, manga, etc.",
+        tags: "peixe salmão salmao cru arroz",
+    },
+    {
+        titulo: "Técnicas para Fazer Sushi",
+        descricao: "Moldagem do sushi com as mãos ou com o uso de um tapete de bambu, corte preciso do peixe e outros ingredientes, preparo do arroz para sushi, montagem de diferentes tipos de sushi (nigiri, maki, uramaki, etc.)",
+        tags: "esteira bambu, enrolar"
+    },
+    {
+        titulo: "Acompanhamentos para Sushi",
+        descricao: "Shoyu (molho de soja), wasabi (raiz forte), gengibre em conserva, missoshiro (sopa de missô), salada de wakame, edamame, etc."
+    },
+    {
+        titulo: "Arranjos para Pratos de Sushi",
+        descricao: "Uso de pratos e tigelas tradicionais japonesas, disposição dos pratos de forma harmoniosa, decoração com flores comestíveis ou outros elementos naturais, uso de molheiras e palitinhos de bambu."
+    }
+];
